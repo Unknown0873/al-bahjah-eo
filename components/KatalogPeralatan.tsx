56c843e01f028c1e18ef7480a53f4a3b03a4956e
Hasan <hasanpskn18@gmail.com>
2026-04-11 15:15:45 +0700

    Fix image path case sensitivity

--- components/KatalogPeralatan.tsx
+++ components/KatalogPeralatan.tsx
@@ -10,10 +10,10 @@ export default function KatalogPeralatan() {
     { name: "AC Portable", desc: "Kami menghadirkan kesejukan yang sesungguhnya. Sebuah solusi pendingin untuk mengalahkan cuaca panas dan menjaga suhu ruangan tetap ideal.", image: "/images/katalog/ac-portable.png" },
     { name: "Paket Lighting", desc: "Kami menjadi solusi pencahayaan lengkap untuk setiap kebutuhan. Dari mewarnai acara dengan PAR LED, menciptakan efek spektakuler dengan moving beam, hingga meningkatkan pencahayaan dengan fresnel.", image: "/images/katalog/paket-lighting.png" },
     { name: "Kipas Angin", desc: "Kami menghadirkan kesejukan di tengah acara Anda. Tersedia dalam berbagai ukuran untuk memastikan kenyamanan para tamu di setiap momen spesial.", image: "/images/katalog/kipas angin.png" },
-    { name: "Panggung Flooring", desc: "Kami sangat cocok untuk berbagai kebutuhan mulai dari acara formal seperti seminar, wisuda, tabligh akbar, pernikahan, hingga acara keagamaan atau pertemuan komunitas.", image: "/images/katalog/panggung Flooring.png" },
+    { name: "Panggung Flooring", desc: "Kami sangat cocok untuk berbagai kebutuhan mulai dari acara formal seperti seminar, wisuda, tabligh akbar, pernikahan, hingga acara keagamaan atau pertemuan komunitas.", image: "/images/katalog/panggung flooring.png" },
     { name: "Panggung Rigging", desc: "Kami menawarkan konstruksi yang kokoh dan aman, dirakit oleh tim ahli untuk menopang semua kebutuhan acara Anda.", image: "/images/katalog/panggung-riging.png" },
     { name: "WC Portable", desc: "Kami menjadi solusi sanitasi yang bersih, nyaman, dan praktis untuk semua jenis acara. Pastikan tamu Anda mendapatkan fasilitas terbaik di manapun lokasi event Anda diselenggarakan.", image: "/images/katalog/WC-Portable.png" },
-    { name: "Kipas Misty Fan", desc: "Kami membuat oase sejuk di mana saja. Dilengkapi 3 tingkat kecepatan dan roda untuk penempatan fleksibel, kipas ini adalah solusi pendingin praktis dan efisien untuk venue indoor maupun outdoor.", image: "/images/katalog/Kipas-Embun.png" },
+    { name: "Kipas Misty Fan", desc: "Kami membuat oase sejuk di mana saja. Dilengkapi 3 tingkat kecepatan dan roda untuk penempatan fleksibel, kipas ini adalah solusi pendingin praktis dan efisien untuk venue indoor maupun outdoor.", image: "/images/katalog/kipas-embun.png" },
   ];
 
   return (
